a6d559af0453b2b9009a4a31a30f6410ad1530e7
mhguo1 <mhguo1@gmail.com>
2018-12-08 13:28:38 -0500

    Update sum_AF.R

--- sum_AF.R
+++ sum_AF.R
@@ -18,8 +18,11 @@ for( i in 2:length(pops)){
 dat$USA_CR<-0.678*dat$NFE_CR+0.019*dat$ASJ_CR+0.122*dat$AFR_CR+0.0175*dat$EAS_CR+0.00997*dat$SAS_CR+0.1533*dat$AMR_CR
 
 #remove bad snps
+faulty<-read.delim("Table_S1.txt", header=T, stringsAsFactors=F, sep="\t")
+faulty$SNP<-paste(faulty$chrom, faulty$pos, faulty$ref, faulty$alt, sep=":")
+
 dat$SNP<-paste(dat$chrom, dat$pos, dat$ref, dat$alt, sep=":")
-dat<-dat[ !(dat$SNP%in%haque_list[,1]) & !(dat$SNP%in%clinvar_list[,1]),]
+dat<-dat[ !(dat$SNP%in%faulty$SNP),]
 
 #Calculate GCRs for each gene
 sum.dat<-data.frame(symbol=unique(dat$symbol), ALL=0, AFR=0,AMR=0, ASJ=0, EAS=0, FIN=0, NFE=0, SAS=0, USA=0, stringsAsFactors = F)
